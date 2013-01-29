d6f6f0d9d9fd8d5e2a9ec3c1103f216990de8461
Brad Chase <bosco87@gmail.com>
2013-01-29 06:07:11 -0500

    get_subtitle_file working

--- scribe.lua
+++ scribe.lua
@@ -24,3 +24,33 @@ function screenshot()
   -- load screenshot
   -- write metadata
 end
+
+function get_video_uri()
+	local video_uri = vlc.input.item():uri()
+	video_uri = vlc.strings.decode_uri(video_uri)
+  return video_uri
+end
+
+function get_subtitle_file()
+  local video_uri = get_video_uri()
+  local extensions = {'.ass','.srt'}
+	local sub_file_name = string.gsub(video_uri, "^.-///(.*)%..-$","%1")
+
+  for index, extension in pairs(extensions) do
+    sub_full_path = sub_file_name..extension
+    file = io.open(sub_full_path,'r')
+
+    if file then 
+      return file 
+    end
+
+    sub_full_path = "/"..sub_file_name..extension
+    file = io.open(sub_full_path,'r')
+
+    if file then 
+      return file 
+    end
+  end
+end
+
+-- subtitle = vlc.var.libvlc_command("libvlc_video_get_spu", vlc.object.libvlc())
