fc1ee2ebaf467589be00b30c8492893e9a94437f
Brad Chase <bosco87@gmail.com>
2013-01-30 06:08:09 -0500

    implement fetching of current subtitle text

--- scribe.lua
+++ scribe.lua
@@ -11,86 +11,101 @@ function descriptor()
   }
 end
 
-subtitles = {}
-
 -- start extention
 function activate()
   --vlc.msg.info("[Scribe] Activating\n")
   -- [ ] register hotkey
   
+  -- [\] parse .ass
+  -- [ ] parse .srt
+  subtitles = parse_subtitles()
+  print(#subtitles)
+
+	time_num      = time_num()
+  sub_delay_num = sub_delay_num()
+
+  sub_time_num  = time_num + sub_delay_num
+
+  sub_text = get_subtitle_for_time_num(sub_time_num, subtitles)
+
   screenshot()
   --input_callback('add')
 end
 
 function screenshot()
   -- [ ] pause
-  -- [.] get subtitle text
+  -- [X] get subtitle text
   -- [X] take screenshot -- vlc.var.command(vlc.object, 'snapshot')
   -- [ ] play
  
   -- [ ] load screenshot
   -- [ ] write metadata
-  -- [ ] POST to server image
   
-  line = "Dialogue: 0,0:00:00.82,0:00:02.42,JP,,0000,0000,0000,,  何言ってんだか"
-  parse_ass_subtitles('/home/bosco/Downloads/sao/sao04.ass')
-  print(#subtitles)
-  print(subtitles[#subtitles-1][3])
+  -- [ ] POST to server image
 end
 
-function input_callback(action)  -- action=add/del/toggle
-  vlc.msg.info("[Scribe] assigning callback\n")
-  -- action = 'add'
-	local input = vlc.object.input()
-  if input then
-    vlc.var.add_callback(input, "intf-event", input_events_handler, "Hello world!")
-    vlc.msg.info("[Scribe] callback assigned\n")
+function get_subtitle_for_time_num(time_num, subtitles)
+  print('get sub for time num')
+  print(time_num)
+  print(#subtitles)
+  for times, text in pairs(subtitles) do
+    start_num  = times[1]
+    finish_num = times[2]
+
+    --print(start_num, finish_num, text)
+    --print(time_num.." >= "..start_num.." and "..time_num.." <= "..finish_time)
+    if (time_num >= start_num) and (time_num <= finish_num) then
+      return text
+    end
   end
 end
 
-function input_events_handler(var, old, new, data)
-  vlc.msg.info("[Scribe] here\n")
-	if new==4 then  -- 4 ~ INPUT_EVENT_POSITION 
-		local input = vlc.object.input()
-		if input then
-      local time = vlc.var.get(input, "time")
-      vlc.msg.info("[Scribe] "..time.."\n")
-		end
-	end
-end
-
 function parse_subtitles()
-  file_path, extension = get_subtitle_file_path()
+  file_path_and_extension = get_subtitle_file_path()
+  file_path = file_path_and_extension[1]
+  extension = file_path_and_extension[2]
+  print(file_path)
+
+  subtitles = {}
+
   if     extension == '.ass' then
     subtitles = parse_ass_subtitles(file_path)
   elseif extension == '.srt' then
     subtitles = parse_srt_subtitles(file_path)
   end
+
+  return subtitles
 end
 
 function parse_ass_subtitles(file_path)
   -- e.g. -- Dialogue: 0,0:00:00.82,0:00:02.42,JP,,0000,0000,0000,,  何言ってんだか
+  subtitles = {}
   
   for line in io.lines(file_path) do
     if string.find(line, "^Dialogue") then
-    timestamp_pattern = "%d+:%d+:%d+\.%d+,%d+:%d+:%d+\.%d+"
+      print(line)
+      timestamp_pattern = "%d+:%d+:%d+\.%d+,%d+:%d+:%d+\.%d+"
 
-    timestamps = string.sub(line, string.find(line, timestamp_pattern))
+      timestamps = string.sub(line, string.find(line, timestamp_pattern))
 
-    start  = string.sub(timestamps, 1, 10)
-    finish = string.sub(timestamps, 12, 21)
+      start  = string.sub(timestamps, 1, 10)
+      finish = string.sub(timestamps, 12, 21)
 
-    start_num  = ass_timestamp_to_number(start)
-    finish_num = ass_timestamp_to_number(finish)
+      start_num  = ass_timestamp_to_number(start)
+      finish_num = ass_timestamp_to_number(finish)
 
-    print(line)
-    text = string.sub(line, string.find(line, ",,.+$"))
+      times = {start_num, finish_num}
 
-    subtitle = {start_num, finish_num, text}
+      text = string.sub(line, string.find(line, ",,.+$"))
 
-    table.insert(subtitles, #subtitles+1, subtitle)
+      --subtitle = {start_num, finish_num, text}
+      --table.insert(subtitles, #subtitles+1, subtitle)
+
+      subtitles[times] = text
     end
   end
+
+  return subtitles
 end
 
 function ass_timestamp_to_number(timestamp)
@@ -124,12 +139,15 @@ function timestamp_to_number(hour, min, sec, mil)
   num = num + (tonumber(min)  * 60)
   num = num + (tonumber(sec))
   num = num + (tonumber("."..mil))
-  print(num)
   return num
 end
 
 function sub_delay_num()
   return vlc.var.get(vlc.object.input(), 'spu-delay')
+end
+
+function time_num()
+	return vlc.var.get(vlc.object.input(), "time")
 end
 
 function get_video_uri()
@@ -138,7 +156,7 @@ function get_video_uri()
   return video_uri
 end
 
-function get_subtitle_file()
+function get_subtitle_file_path()
   local video_uri = get_video_uri()
   local extensions = {'.ass','.srt'}
   local sub_file_name = string.gsub(video_uri, "^.-///(.*)%..-$","%1")
@@ -160,7 +178,28 @@ function get_subtitle_file()
   end
 end
 
+--function input_callback(action)  -- action=add/del/toggle
+--  vlc.msg.info("[Scribe] assigning callback\n")
+--  -- action = 'add'
+--	local input = vlc.object.input()
+--  if input then
+--    vlc.var.add_callback(input, "intf-event", input_events_handler, "Hello world!")
+--    vlc.msg.info("[Scribe] callback assigned\n")
+--  end
+--end
+--
+--function input_events_handler(var, old, new, data)
+--  vlc.msg.info("[Scribe] here\n")
+--	if new==4 then  -- 4 ~ INPUT_EVENT_POSITION 
+--		local input = vlc.object.input()
+--		if input then
+--      local time = vlc.var.get(input, "time")
+--      vlc.msg.info("[Scribe] "..time.."\n")
+--		end
+--	end
+--end
+
 -- sub_delay = vlc.var.get(vlc.object.input(), 'spu-delay')
 -- subtitle = pcall(vlc.var.libvlc_command, libvlc_video_get_spu)
 
-activate()
+--activate()
